5357f28c9746aa46faa8bda37fb635841ffe114e
Divider <it-cio@mail.ru>
2024-08-18 15:26:25 +0300

    Add tune func

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import sys
 import os
 from os import getenv
 
+import cv2
+import numpy as np
+
 import aioschedule
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher, types
@@ -24,6 +27,30 @@ async def photo():
         for file in files:
             if file.lower().endswith(tuple(ext)) and '~$' not in file:
                 photo_file = os.path.join(media_dir, file)
+                image_original = cv2.imread(photo_file)
+
+                contrast = 1.1
+                brightness = -20
+                image = cv2.convertScaleAbs(image_original, alpha=contrast, beta=brightness)
+                kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
+                image = cv2.filter2D(image, -1, kernel)
+                hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+                h, s, v = cv2.split(hsv)
+                h += 1
+                s += 10
+                v += 0
+                final_hsv = cv2.merge((h, s, v))
+                image = cv2.cvtColor(final_hsv, cv2.COLOR_HSV2BGR)
+
+                face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+                gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+                faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5)
+                for (x, y, w, h) in faces:
+                    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 1)
+
+                image_final = cv2.vconcat([image_original, image])
+                cv2.imwrite(photo_file, image_final)
+
                 await bot.send_photo(chat_id=ID, photo=types.FSInputFile(photo_file))
                 os.remove(os.path.join(media_dir, file))
 
